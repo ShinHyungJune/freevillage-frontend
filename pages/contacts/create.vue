95f6dff732161b4d6b75428e770c56349f400fd8
Dongmin <111101111@naver.com>
2023-02-14 15:31:12 +0900

    내 정보 - 내 마을 활동 - 전화/ 문자 기능, draggable 기능

--- pages/contacts/create.vue
+++ pages/contacts/create.vue
@@ -44,7 +44,35 @@
 
                 <p class="m-comment type02" v-if="!form.info">* 하단 의원 사진 버튼을 눌러 이미지를 등록해주세요.</p>
 
-                <div class="items">
+                <draggable
+                    class="items" v-model="item.data" @input="reorder" v-if="items.data.length >0"
+                > 
+                    <div class="item" v-for="item in items.data" :key="item.id">
+                        <div class="item-top">
+                            <h3 class="title">{{item.phone}}</h3>
+
+                            <button class="btn-remove red" @click="remove(item)">삭제</button>
+                        </div>
+
+                        <div class="img-wrap" v-if="item.img">
+                            <a :href="`tel:${item.phone}`" class="btn-call" v-if="item.phone">
+                                <img src="/images/call.png" alt="" style="width:17px;">
+                            </a>
+                            <div class="m-board-btns mt-20">
+                                <div class="m-btns type01">
+                                    <div class="m-btn-wrap">
+                                        <a :href="`tel:${item.phone}`" class="m-btn type01 bg-revert-primary">연락하기</a>
+                                    </div>
+                                    <div class="m-btn-wrap">
+                                        <a :href="`sms:${item.phone}`" class="m-btn type01 bg-primary">문자하기</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <img :src="item.img.url" alt="" v-if="item.img">
+                        </div>
+                    </div>
+                </draggable>
+                <!-- <div class="items">
                     <div class="item" v-for="item in items.data" :key="item.id">
                         <div class="item-top">
                             <h3 class="title">{{item.name}}</h3>
@@ -56,12 +84,20 @@
                             <a :href="`tel:${item.phone}`" class="btn-call" v-if="item.phone">
                                 <img src="/images/call.png" alt="" style="width:17px;">
                             </a>
-
+                            <div class="m-board-btns mt-20">
+                                <div class="m-btns type01">
+                                    <div class="m-btn-wrap">
+                                        <a :href="`tel:${item.phone}`" class="m-btn type01 bg-revert-primary">연락하기</a>
+                                    </div>
+                                    <div class="m-btn-wrap">
+                                        <a :href="`sms:${item.phone}`" class="m-btn type01 bg-primary">문자하기</a>
+                                    </div>
+                                </div>
+                            </div>
                             <img :src="item.img.url" alt="" v-if="item.img">
                         </div>
-
                     </div>
-                </div>
+                </div> -->
                 <div class="mt-40"></div>
             </div>
         </div>
@@ -78,6 +114,7 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable';
 import InputCamera from '../../components/form/posts/inputCamera';
 import InputLink from "../../components/form/posts/inputLink";
 import InputImg from "../../components/form/posts/inputImg";
@@ -86,7 +123,7 @@ import InputAddress from "../../components/form/inputAddress";
 import Form from "@/utils/Form";
 
 export default {
-    components: {InputAddress, InputThumbnail, InputImg, InputLink, InputCamera},
+    components: {draggable,InputAddress, InputThumbnail, InputImg, InputLink, InputCamera},
     auth: true,
     data() {
         return {
@@ -112,6 +149,16 @@ export default {
     methods: {
         move(e){
             this.$router.push(e.target.value)
+        },
+
+        //TODO 여기서 즉각 api call 해서 바로 순서 저장 가능해야함
+        //TODO 각 항목 수정 저장 가능하게 고쳐야함 
+        reorder(items){
+            this.$emit("reorder", items.map((item, index) => {
+                item.order = index;
+
+                return item;
+            }));
         },
 
         store() {
