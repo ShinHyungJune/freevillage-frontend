b2ffc02e7124aab6ad41e47056a4818c069ac4be
puridev1155 <puridev1155@gmail.com>
2023-03-09 10:02:03 +0900

    가입하기 - 문제

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -375,9 +375,9 @@
 
                         <p class="m-input-error" v-if="errors.district_id" v-text="errors.district_id[0]"></p>
 
-                        <div class="mt-8">
+                      <!-- <div class="mt-8">
                             <span style="color: red">*</span>행정동을 모르실 경우 찾기 버튼을 누르세요.  <button style="color: #0f38bd;" v-touch:tap="active">찾기</button>
-                        </div>
+                        </div> -->
                     </div>
 
                     <div class="mt-16"></div>
