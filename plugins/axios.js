0d62e65b7e720f062f68387dc87166652d9c1b87
ShinHyungJune <ssa4141@naver.com>
2023-04-03 12:32:19 +0900

    401에러시 로그아웃처리 삭제

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function (context) {
-    context.$axios.onError((error) => {
+    /*context.$axios.onError((error) => {
         if (error.response.status === 401) {
             location.href = "/auth/logout"
         }
-    })
+    })*/
 }
